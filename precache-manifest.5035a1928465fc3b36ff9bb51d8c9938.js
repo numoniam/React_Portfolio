40c4102daae4620ffb28e947dafb802d08fc059f
numoniam <savanghori1203@gmail.com>
2022-06-27 09:53:17 +0530

    Updates

---
+++ precache-manifest.5035a1928465fc3b36ff9bb51d8c9938.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aee19f63b73e64185d79",
+    "url": "/numoniam/React_Portfolio.git/static/css/main.140ef9e8.chunk.css"
+  },
+  {
+    "revision": "aee19f63b73e64185d79",
+    "url": "/numoniam/React_Portfolio.git/static/js/main.aee19f63.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/numoniam/React_Portfolio.git/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "8ba31d22c2cbdcc419ad",
+    "url": "/numoniam/React_Portfolio.git/static/js/runtime~main.8ba31d22.js"
+  },
+  {
+    "revision": "92970e0c55681aa13076ad9ec3911dfd",
+    "url": "/numoniam/React_Portfolio.git/index.html"
+  }
+];
